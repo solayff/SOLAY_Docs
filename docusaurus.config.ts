fba5e50a1a5eb3b2bf873903a3c720b8b0579acf
solayff <140053949+solayff@users.noreply.github.com>
2025-05-02 19:27:03 +0300

    deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'solayff', // Usually your GitHub org/user name.
-  projectName: 'SOLAY_Games_Docs', // Usually your repo name.
+  projectName: 'SOLAY_Docs', // Usually your repo name.
 	deploymentBranch: "gh-pages",
 
   onBrokenLinks: 'throw',
